a1de6871c67ec94c4781efe3341a74c299c8f614
Humoyun Akbaraliev <humoyunswe@Humoyun.local>
2024-02-01 19:20:29 +0500

    Changed settings

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4dn($x*c*p8ory^$_-yhvxl)t7!!$9koo89pup^zsw-y*xp_9w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '127.0.0.1', '.vercel.app']
 
